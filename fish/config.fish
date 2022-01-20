2d46671d17cfc08e366e2d345c6c22e2021ff2f3
rokoala <koga.rodrigo@gmail.com>
2022-01-20 17:20:15 -0300

    feat(fish): fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
-source ~/.config/fish/private_functions/**.fish
-# init starship
-# starship init fish | source
+set fish_greeting ""
+set -U fisher_path ~/.config/fish/fisher
+set -U fish_private_functions ~/.config/fish/private_functions
+
+for file in $fish_private_functions/*.fish
+    source $file
+end
