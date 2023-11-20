bcc3c873a21b615c7f363b03cc0ff94b6c114f39
rokoala <koga.rodrigo@gmail.com>
2023-11-19 23:11:00 -0300

    (feat): lazyvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,40 @@
 -- Add any additional options here
 vim.g.mapleader = " "
 
+-- vimwiki
+vim.g.vimwiki_list = {
+  {
+    path = "~/vimwiki",
+    syntax = "markdown",
+    ext = ".md",
+    auto_generate_tags = 1,
+    auto_generated_links = 1,
+    template_path = "~/vimwiki/templates/",
+    template_default = "default",
+    path_html = "~/vimwiki/site_html/",
+    custom_wiki2html = "vimwiki_markdown",
+    html_file_parameterization = 1,
+    template_ext = ".tpl",
+    auto_toc = 1,
+    auto_export = 1,
+  },
+  {
+    path = "~/vimwiki-work",
+    syntax = "markdown",
+    ext = ".md",
+    auto_generate_tags = 1,
+    auto_generated_links = 1,
+    template_path = "~/vimwiki-work/templates/",
+    template_default = "default",
+    path_html = "~/vimwiki-work/site_html/",
+    custom_wiki2html = "vimwiki_markdown",
+    html_file_parameterization = 1,
+    template_ext = ".tpl",
+    auto_toc = 1,
+    auto_export = 1,
+  },
+}
+
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
